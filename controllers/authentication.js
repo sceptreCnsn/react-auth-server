4d41d41a082e6392379b4e07db42da9e8cb103b9
sceptreCnsn <cansin.memis@clb.lcl>
2019-02-12 11:01:43 +0300

    Last commit for server

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,29 +1,49 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
 
-exports.signup = function (req, res, next) {
-    //See if a user with given email exists
-    console.log('Coming Sign-up request', req.body);
-    const email = req.body.email;
-    const password = req.body.password;
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
-    User.findOne({ email: email }, function (err, user) {
-        if (err) { return next(err) };
+exports.signup = function(req, res, next) {
+  //See if a user with given email exists
+  console.log('Coming Sign-up request', req.body);
+  const email = req.body.email;
+  const password = req.body.password;
 
-        //If a user with email does exist return an error
-        if (user) { return res.status(422); }
+  if (!email || !password) {
+    return res.status(422).send({ error: 'E-Mail and Password required.' });
+  }
 
-        // If a user with email doenst exist, create and save user record
-        const newUser = new User({
-            email: email,
-            password: password
-        });
+  User.findOne({ email: email }, function(err, user) {
+    if (err) {
+      return next(err);
+    }
 
-        newUser.save(function(err){
-            if(err) {return next(err);}
+    //If a user with email does exist return an error
+    if (user) {
+      return res.status(422);
+    }
 
-            res.json({success: true});
-        });
+    // If a user with email doenst exist, create and save user record
+    const newUser = new User({
+      email: email,
+      password: password
     });
 
-    //respond to request indicating the user was created.
-}
+    newUser.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      //respond to request indicating the user was created.
+      res.json({ success: true, token: tokenForUser(newUser) });
+    });
+  });
+};
+
+exports.signin = function(req, res, next) {
+  res.send({ success: true, token: tokenForUser(req.user) });
+};
--- models/user.js
+++ models/user.js
@@ -1,17 +1,55 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcrypt-nodejs');
 //define model
 const userSchema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        lowercase: true
-    },
-    password: String
+  email: {
+    required: true,
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    required: true,
+    type: String
+  }
 });
 
-//create the model class 
+//On save hook encrypt password
+//Before saving a model run this function
+userSchema.pre('save', function(next) {
+  //get access to the user model
+  const user = this;
+
+  //generate a salt then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    // hash our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      //overwrite plaint text pw with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePw, callback) {
+  bcrypt.compare(candidatePw, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
+};
+
+//create the model class
 const ModelClass = mongoose.model('user', userSchema);
 
 //export the model
